17363b8afccc6418d67c6f413c345ac5d55982e3
LamaVi2000 <dthjif2010@mail.ru>
2022-10-06 02:41:30 +1000

    Особенный пользователь

---
+++ Lections/Lesson_1/Example_6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Маша" )
+{
+
+    Console.WriteLine(" УРА " );
+}
+else 
+{
+
+Console.Write("Привет ");
+Console.Write(username);
+}
