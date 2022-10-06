aa4c62d4e1f1354829203cb1577f11d18b9d92ca
LamaVi2000 <dthjif2010@mail.ru>
2022-10-06 22:30:31 +1000

    Определение наибольшего числа

---
+++ Homeworks/Lesson 1/Example_1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число ");
+
+int a = Convert.ToInt32(Console.ReadLine()) ;
+
+Console.Write("Введите второе число ");
+
+int b= Convert.ToInt32(Console.ReadLine()) ;
+
+ bool Sit0 = a > b;
+bool Sit1 = a == b;
+bool Sit2 = a < b;
+ 
+if (Sit0)
+    {
+        Console.WriteLine(" Наибольшее число ");
+        Console.WriteLine(a);
+            }
+     else if (Sit1)
+      {
+                Console.WriteLine("Числа равны");
+            }
+     else
+        {
+                Console.WriteLine(" Наибольшее число " );
+                Console.WriteLine(b);
+            }
+
+
+
