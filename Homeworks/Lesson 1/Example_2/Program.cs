16a9391275ba944b551d0ef7cf96aeedc0d61231
LamaVi2000 <dthjif2010@mail.ru>
2022-10-06 23:05:56 +1000

    Выбор наибольшего числа из трех

---
+++ Homeworks/Lesson 1/Example_2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Введите первое число ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число ");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+
+if (b > max)
+{
+    max = b;
+}
+
+else if (c > max)
+{
+    max = c ;
+}
+
+Console.WriteLine("Наибольшее число ");
+Console.WriteLine(max);
+
