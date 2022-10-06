db89eb305e1a87118085f9c418807455c815e922
LamaVi2000 <dthjif2010@mail.ru>
2022-10-06 23:37:08 +1000

    Определение четного и нечетного числа

---
+++ Homeworks/Lesson 1/Example_3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a %2 == 0) 
+{
+    
+    Console.WriteLine(" Число чётное ") ;
+}
+    
+
+ else 
+ { 
+    Console.WriteLine(" Число нечетное ");
+ }
+ 
+
+
+
+
+
