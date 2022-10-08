1ba231ca66f75c1e5df770fcc6380d40c9c35ab4
LamaVi2000 <dthjif2010@mail.ru>
2022-10-08 16:22:15 +1000

    Вывод второй цифры числа

---
+++ Homeworks/Lesson_2/Example_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите метод, который принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+
+ int number = Convert.ToInt32(Console.ReadLine()) ;
+
+int SecondCount(int num)
+{
+
+return num / 10 % 10 ;
+
+}
+
+Console.WriteLine(SecondCount(number));
+
+
