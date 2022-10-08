4682dc8dc0e477ea502dd86cfc12dfd223fdabb1
LamaVi2000 <dthjif2010@mail.ru>
2022-10-08 22:52:26 +1000

    Вывод третьей цифры

---
+++ Homeworks/Lesson_2/Example_2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 13: Напишите метод, который выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ThirdNumber ( int num )
+{
+    if (num >= 100)
+    {
+      Console.WriteLine(num% 10);
+    }
+    else if (num < 100)
+    {
+        Console.WriteLine("Нет цифры");
+    }  return num;
+}
+
+ThirdNumber(number);
+
+
