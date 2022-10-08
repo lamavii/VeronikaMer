d0af84146870b605b2be0d0ad360bc9094df05ca
LamaVi2000 <dthjif2010@mail.ru>
2022-10-08 22:45:40 +1000

    Дни недели

---
+++ Homeworks/Lesson_2/Example_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 15: Напишите метод, который принимает на вход цифру, 
+//обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число от 1 до 7 включительно");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void DayNumber(int day)
+{
+if(day==6 ||day == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else Console.WriteLine("Будний") ;
+}
+
+DayNumber(number);
+
+
+
+//if(DayNumber(number)==6 ||DayNumber(number)==7 ) Console.WriteLine("Выходной");
+//else Console.WriteLine("Будний");
+
