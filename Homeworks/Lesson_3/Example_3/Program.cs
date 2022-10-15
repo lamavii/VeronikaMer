fce473c6b1dea00fbf713f274a276aae790c362a
LamaVi2000 <dthjif2010@mail.ru>
2022-10-15 13:24:21 +1000

    Куб числа

---
+++ Homeworks/Lesson_3/Example_3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int number = 0;
+
+    Console.Write("Введите натуральное число больше единицы");
+    number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("  N  |  N^3  ");
+
+for (int i = 1; i <= number; i++)
+{
+        Console.Write($"{i,3}  |");
+        Console.WriteLine($"  {i * i * i, 3}");
+}
