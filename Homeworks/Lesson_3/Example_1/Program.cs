a98cb2faee72c6a2e3894345c5b2a20fbba10fce
LamaVi2000 <dthjif2010@mail.ru>
2022-10-15 13:18:02 +1000

    Палиндром

---
+++ Homeworks/Lesson_3/Example_1/Program.cs
@@ -0,0 +1,41 @@
+﻿
+int number = 0;
+    Console.Write("Введите натуральное число");
+    number = Convert.ToInt32(Console.ReadLine());
+    
+if (number < 10)
+{
+    Console.WriteLine($"Число {number} является палиндромом.");
+    return;
+}
+
+string Palindrom(int num)
+{
+    int i = 0;
+    int diff = num;
+    do                                   
+    {
+        diff = diff / 10;
+        i++;
+    } while (diff > 0);
+    int[] cifri = new int[i];            
+
+    i = 0;
+    do                                   
+    {
+        cifri[i] = num % 10;
+        num = num / 10;
+        i++;
+    } while (num > 0);
+
+    for (i = 0; (i < (cifri.Length / 2)); i++) 
+    {
+        if (cifri[i] != cifri[cifri.Length - (1 + i)]) 
+        {
+            return "не является палиндромом";
+        }
+    }
+    return "является палиндромом";
+}
+
+Console.WriteLine($"Число {number} {Palindrom(number)}");
