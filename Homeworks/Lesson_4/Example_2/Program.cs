e0746e5905bc3ec1cee15083190f94496561950f
LamaVi2000 <dthjif2010@mail.ru>
2022-10-16 17:24:52 +1000

    Сумма цифр в числе

---
+++ Homeworks/Lesson_4/Example_2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*  Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе. */
+
+int num1=0;
+Console.Write("Введите целое положительное число");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+    int Sum(int num)
+{
+    int summa = 0;
+    for (int i = 0; num > 0; i++)
+    {
+        summa = summa + (num % 10);
+        num = num / 10;
+    }
+    return summa;
+}
+
+int result = Sum(num1);
+Console.WriteLine($"Сумма цифр числа {num1} равна {result}");
+
