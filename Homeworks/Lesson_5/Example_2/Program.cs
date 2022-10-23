37a6aa1f5f253e74927d77d8e709b5b5ac4fd592
LamaVi2000 <dthjif2010@mail.ru>
2022-10-24 09:05:50 +1000

    Сумма нечетных элементов в массиве

---
+++ Homeworks/Lesson_5/Example_2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.*/
+
+int[] RandomArray(int length)
+{
+    Random rnd = new Random();
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(-100, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < (arr.Length - 1); i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}");
+    Console.Write("]");
+}
+
+int SumNeChet(int[] arr)
+{
+    int count = 0;
+    for (int i = 1; i < arr.Length; i+=2)
+    {
+        count += arr[i];
+    }
+    return count;
+
+}
+
+int size = 0;
+Console.Write("Введите длину массива");
+size = Convert.ToInt32(Console.ReadLine());
+int[] mass = RandomArray(size);
+PrintArray(mass);
+int res=SumNeChet(mass);
+Console.WriteLine($" -> сумма нечетных элементов массива равна {res}");
