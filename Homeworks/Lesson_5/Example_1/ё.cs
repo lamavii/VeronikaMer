6972d50d88ad0a3aed57ff55bc04939f8bce4b37
LamaVi2000 <dthjif2010@mail.ru>
2022-10-24 08:12:06 +1000

    программа показывает четное количество чисел в массиве

---
+++ Homeworks/Lesson_5/Example_1/ё.cs
@@ -0,0 +1,45 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+
+int[] RandomArray(int length)
+{
+    Random rnd = new Random();
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < (arr.Length - 1); i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}");
+    Console.Write("]");
+}
+
+int CountChet(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int size = 0;
+    Console.Write("Введите длину массива");
+    size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] mass = RandomArray(size);
+PrintArray(mass);
+int res = CountChet(mass);
+Console.WriteLine($" -> количество четных элементов массива равняется {res}");
