db727734170de69f4dc9516687b7a9cb78c4e66b
LamaVi2000 <dthjif2010@mail.ru>
2022-10-24 23:57:43 +1000

    Положительные числа

---
+++ Homeworks/Lesson_6/Example_1/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int[] CreateAndManuallyFillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write("Введите значения");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < (arr.Length - 1); i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}");
+    Console.Write("]");
+}
+
+int CountPositiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите количество вводимых значений: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = CreateAndManuallyFillArray(size);
+PrintArray(numbers);
+int result = CountPositiveNumbers(numbers);
+Console.WriteLine($" -> Пользователь ввел {result} положительных чисел.");
